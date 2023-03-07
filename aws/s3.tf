be90f3b2099cf8bea72a26f3c6d9b42dab85c392
Gordon Brandyburg <gbrandyburg@paloaltonetworks.com>
2023-03-07 11:12:53 -0500

    Latest tags applied

--- aws/s3.tf
+++ aws/s3.tf
@@ -12,9 +12,9 @@ resource "aws_s3_bucket" "data" {
     Name                 = "${local.resource_prefix.value}-data"
     Environment          = local.resource_prefix.value
     yor_trace            = "0c23f4fd-6fd9-4ed1-925f-6c1b13388837"
-    git_commit           = "N/A"
+    git_commit           = "fafad960053fe30d4097f952b8e2be0a3bd1e936"
     git_file             = "aws/s3.tf"
-    git_last_modified_at = "2023-03-07 16:11:07"
+    git_last_modified_at = "2023-03-07 16:12:20"
     git_last_modified_by = "gbrandyburg@paloaltonetworks.com"
     git_modifiers        = "gbrandyburg/matt"
     git_org              = "adopt-guide-demos-US2"
@@ -35,11 +35,11 @@ resource "aws_s3_bucket" "financials" {
     Name                 = "${local.resource_prefix.value}-financials"
     Environment          = local.resource_prefix.value
     yor_trace            = "b1450859-3ab4-43db-b287-c19034fe0fbb"
-    git_commit           = "8aefc18757655c5685aca4a12b2a1a179f620ee1"
+    git_commit           = "fafad960053fe30d4097f952b8e2be0a3bd1e936"
     git_file             = "aws/s3.tf"
-    git_last_modified_at = "2021-06-16 14:25:48"
-    git_last_modified_by = "matt@bridgecrew.io"
-    git_modifiers        = "matt"
+    git_last_modified_at = "2023-03-07 16:12:20"
+    git_last_modified_by = "gbrandyburg@paloaltonetworks.com"
+    git_modifiers        = "gbrandyburg/matt"
     git_org              = "adopt-guide-demos-US2"
     git_repo             = "deployablegoat"
   }
@@ -60,11 +60,11 @@ resource "aws_s3_bucket" "operations" {
     Name                 = "${local.resource_prefix.value}-operations"
     Environment          = local.resource_prefix.value
     yor_trace            = "4534313c-6d0d-457d-b928-4c89689fb08a"
-    git_commit           = "8aefc18757655c5685aca4a12b2a1a179f620ee1"
+    git_commit           = "fafad960053fe30d4097f952b8e2be0a3bd1e936"
     git_file             = "aws/s3.tf"
-    git_last_modified_at = "2021-06-16 14:25:48"
-    git_last_modified_by = "matt@bridgecrew.io"
-    git_modifiers        = "matt"
+    git_last_modified_at = "2023-03-07 16:12:20"
+    git_last_modified_by = "gbrandyburg@paloaltonetworks.com"
+    git_modifiers        = "gbrandyburg/matt"
     git_org              = "adopt-guide-demos-US2"
     git_repo             = "deployablegoat"
   }
